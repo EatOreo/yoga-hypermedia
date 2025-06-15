850a73b22ef4d3b856444f3e98ae727ed7f25424
LucasNK <lkhac2001@gmail.com>
2025-06-15 12:59:26 +0200

    again

--- pages/teachers/[name].vue
+++ pages/teachers/[name].vue
@@ -4,36 +4,34 @@
       { label: 'Teachers', to: '/teachers' },
       { label: name }
     ]" />
-    <div class = "flex justify-center items-start">
-    <img :src="teacherItem.image" alt="Huy Tran Vin" class="w-40 h-40 rounded-full shadow-md">
-    </div>
+    <img class = "flex justify-center items-start mb-4 w-40 h-40 rounded-full shadow-md" src="teacherItem.image" alt="">
     <Background>
-        <div class="container flex-wrap mx-auto gap-10 p-6">        
-          <div class="flex flex-col items-center md:items-start">
-            <div class="md:ml-6 mt-4 md:mt-0">
-              <h3 class="text-2xl text-teal-900 font-semibold">{{ teacherItem?.name }}</h3>
-              <p class="italic text-yellow-300 mt-2">{{ teacherItem?.quote }}</p>
-              <p class="mt-4 text-gray-700">{{ teacherItem?.description }}</p>
-            </div>
-            <div class="mt-6">
-            <h4 class="text-xl emerald-600 font-semibold">Contact (email)</h4>
-            <p class="text-gray-600">{{ teacherItem?.email }}</p>
-            </div>
+      <div class="container flex-wrap mx-auto gap-10 p-6">
+        <div class="flex flex-col items-center md:items-start">
+          <div class="mt-4">
+            <h3 class="text-2xl text-teal-900 font-semibold">{{ teacherItem?.name }}</h3>
+            <p class="italic text-yellow-300 mt-2">{{ teacherItem?.quote }}</p>
+            <p class="mt-4 text-gray-700">{{ teacherItem?.description }}</p>
           </div>
-          <div class="flex flex-row justify-start items-center flex-nowrap gap-4">
-            <div>
-              <h3 class="text-xl text-center emerald-600 font-semibold">Classes</h3>
-            </div>
-            <div class="flex flex-row gap-4 overflow-x-auto">
-              <ClassCard v-for="classItem in classes.filter(c => c.teacherId === teacherItem!.id)" :key="classItem.title"
-              :title="classItem.title" :subtitle="classItem.subtitle" :intensity="classItem.intensity" :image="classItem.image" />
-            </div>
+        </div>
+        <div class="mt-6">
+          <h4 class="text-xl emerald-600 font-semibold">Contact (email)</h4>
+          <p class="text-gray-600">{{ teacherItem?.email }}</p>
+        </div>
+      </div>
+        <div class="flex flex-row justify-start items-center flex-nowrap gap-4">
+          <div>
+            <h3 class="text-xl text-center emerald-600 font-semibold">Classes</h3>
           </div>
-          <div class="flex flex-row justify-start items-center flex-nowrap gap-4">
-            <h3 class="text-xl text-center emerald-600 font-semibold">Events</h3>
-            <div class="flex flex-row gap-4 overflow-x-auto">
-              <EventCard v-for="eventItem in events.filter(e => e.teacherIds?.includes(teacherItem!.id))" :key="eventItem.title" :event="eventItem" />
-            </div>
+          <div class="flex flex-row gap-4 overflow-x-auto">
+            <ClassCard v-for="classItem in classes.filter(c => c.teacherId === teacherItem!.id)" :key="classItem.title"
+            :title="classItem.title" :subtitle="classItem.subtitle" :intensity="classItem.intensity" :image="classItem.image" />
+          </div>
+        </div>
+        <div class="flex flex-row justify-start items-center flex-nowrap gap-4">
+          <h3 class="text-xl text-center emerald-600 font-semibold">Events</h3>
+          <div class="flex flex-row gap-4 overflow-x-auto">
+            <EventCard v-for="eventItem in events.filter(e => e.teacherIds?.includes(teacherItem!.id))" :key="eventItem.title" :event="eventItem" />
           </div>
         </div>
     </BackGround>
