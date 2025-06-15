a1803558f5e3e1bacef6ea95cc72ff3154d4aefe
LucasNK <lkhac2001@gmail.com>
2025-06-15 19:09:45 +0200

    better teacher bio display

--- pages/teachers/[name].vue
+++ pages/teachers/[name].vue
@@ -9,14 +9,14 @@
     </div>
     <Background>
       <div class="flex flex-row items-center mx-auto gap-5 p-4 border-b-2 border-lgreen">
-        <div class="flex flex-col items-center md:items-start mb-4">
+        <div class="flex w-2/3 flex-col items-center md:items-start mb-4">
           <div class="mt-4">
             <h3 class="text-dark text-[32px]">{{ teacherItem?.name }}</h3>
             <p class="text-lgreen text-sm">{{ teacherItem?.quote }}</p>
             <p class="text-dark pt-4 text-justify">{{ teacherItem?.description }}</p>
           </div>
         </div>
-        <div class="flex flex-col items-start mb-4">
+        <div class="flex w-1/3 flex-col items-center justify-center mb-4">
           <p class="text-lgreen text-align text-lg">Contact</p>
           <p class="border rounded-full px-4 py-0 opacity-100
           shadow-md text-md lowercase font-bold bg-lgreen text-white-100 shadow-white-500/50">{{ teacherItem?.email }}
