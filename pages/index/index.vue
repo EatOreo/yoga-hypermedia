2980d1cfc228f26f746de271fad90f044622ab48
luhj <luhj@itu.dk>
2025-06-17 15:46:19 +0200

    move contacts

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,12 +3,14 @@
         <div class="container mx-auto px-4 pb-20 md:py-20">
             <div class="flex flex-col md:flex-row items-center justify-between">
                 <div class="md:w-2/3 mb-10 md:mb-0">
-                    <div class="flex flex-col items-center justify-between pt-12 md:pt-20 px-8">
+                    <div class="flex flex-col items-center justify-between pt-12 md:pt-20 px-8" :class="[
+                        'transition-opacity duration-700',
+                        show ? 'opacity-100' : 'opacity-0'
+                    ]">
                         <h1 class="text-4xl md:text-5xl font-bold mb-2">Find Your Inner Peace with Yogaga</h1>
                         <p class="text-lgreen text-md">Discover the perfect balance of mind, body, and soul through
-                            our
-                            expert-led yoga classes.</p>
-                        <div class="flex gap-4 pt-4 justify-end">
+                            our expert-led yoga classes.</p>
+                        <div class="flex gap-8 pt-4 justify-end">
                             <NuxtLink to="/classes" class="text-sm text-lblue italic">Start Your Journey</NuxtLink>
                             <NuxtLink to="/aboutus" class="text-sm text-lblue italic">Learn More</NuxtLink>
                         </div>
@@ -28,9 +30,17 @@
 
     <section class="bg-primary/5 py-20">
         <div class="container mx-auto px-4 text-center">
-            <h2 class="text-3xl font-bold mb-6">Ready to Begin Your Yoga Journey?</h2>
-            <p class="text-xl mb-8">Join us today and experience the transformative power of yoga.</p>
+            <h2 class="text-3xl font-bold mb-2">Ready to Begin Your Yoga Journey?</h2>
+            <p class="text-lgreen text-md mb-4">Join us today and experience the transformative power of yoga.</p>
             <NuxtLink to="/classes" class="text-sm text-lblue italic">Explore Classes</NuxtLink>
         </div>
     </section>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const show = ref(false)
+onMounted(() => {
+    show.value = true
+})
+</script>
