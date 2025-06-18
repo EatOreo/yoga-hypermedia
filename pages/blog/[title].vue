d567ee2a4af4588f82b6f251315aab467cfe5946
CarixoHD <shayking7yt@gmail.com>
2025-06-18 15:13:54 +0200

    fix blog for phone

--- pages/blog/[title].vue
+++ pages/blog/[title].vue
@@ -5,11 +5,11 @@
       { label: post.title }
     ]" />
     <Background>
-    <div class="pt-16 px-64 flex flex-col items-center">
+    <div class="pt-8 md:pt-16 px-4 sm:px-8 md:px-16 lg:px-32 flex flex-col items-center">
       <img class="rounded-2xl" :src="post.image">
       <p class="text-sm text-lgreen italic pt-2"> {{ post.author }}, {{ post.date }}</p>
     </div>
-      <div class="px-32 py-16" v-html="post.body"></div>
+      <div class="px-4 sm:px-8 md:px-16 lg:px-32 py-8 md:py-16" v-html="post.body"></div>
     </Background>
   </div>
 </template>
